feb05fdd240969d10c2729d2f3f368da340a245a
Rafa Huelin <it@engisa.com>
2017-07-10 11:57:27 +0100

    Create simple test case using expect assertion library

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -1,3 +1,5 @@
+import expect from 'expect';
+
 const add = (a, b) => {
   if (typeof b !== 'number') {
     return a + a;
@@ -12,17 +14,13 @@ describe('add', function () {
   it('should add two numbers', function () {
     const res = add(11, 9);
 
-    if (res !== 20) {
-      throw new Error('Sum was not equal to expeted value');
-    }
+    expect(res).toBe(20);
   });
 
   it('should double a single number', function () {
     const res = add(44);
 
-    if (res !== 88) {
-      throw new Error('Number was not doubled.');
-    }
+    expect(res).toBe(88);
   });
 });
 
@@ -30,8 +28,6 @@ describe('square', function () {
   it('should square a number', function () {
     const res = square(5);
 
-    if (res !== 25) {
-      throw new Error('Number was not squared');
-    }
+    expect(res).toBe(25);
   });
 });

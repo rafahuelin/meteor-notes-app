a69b7e2af54741b0083b47d9e002df36ad685623
Rafa Huelin <it@engisa.com>
2017-07-10 12:31:14 +0100

    Test valid email

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -1,33 +1,70 @@
+import { Meteor } from 'meteor/meteor';
 import expect from 'expect';
 
-const add = (a, b) => {
-  if (typeof b !== 'number') {
-    return a + a;
-  }
+import { validateNewUser } from './users';
 
-  return a + b;
-};
+if (Meteor.isServer) {
+  describe('users', function () {
 
-const square = (a) => a * a;
+    it('should allow valid email address', function () {
+      const testUser = {
+        emails: [
+          {
+            address: 'test@example.com'
+          }
+        ]
+      };
+      const res = validateNewUser(testUser);
 
-describe('add', function () {
-  it('should add two numbers', function () {
-    const res = add(11, 9);
+      expect(res).toBe(true);
+    });
 
-    expect(res).toBe(20);
+    it('should reject invalid email', function () {
+      expect(() => {
+        const testUser = {
+          emails: [
+            {
+              address: 'tesxamplecom'
+            }
+          ]
+        };
+
+        validateNewUser(testUser);
+      }).toThrow();
+    });
   });
+}
 
-  it('should double a single number', function () {
-    const res = add(44);
 
-    expect(res).toBe(88);
-  });
-});
 
-describe('square', function () {
-  it('should square a number', function () {
-    const res = square(5);
-
-    expect(res).toBe(25);
-  });
-});
+// const add = (a, b) => {
+//   if (typeof b !== 'number') {
+//     return a + a;
+//   }
+//
+//   return a + b;
+// };
+//
+// const square = (a) => a * a;
+//
+// describe('add', function () {
+//   it('should add two numbers', function () {
+//     const res = add(11, 9);
+//
+//     expect(res).toBe(20);
+//   });
+//
+//   it('should double a single number', function () {
+//     const res = add(44);
+//
+//     expect(res).toBe(88);
+//   });
+// });
+//
+// describe('square', function () {
+//   it('should square a number', function () {
+//     const res = square(5);
+//
+//     expect(res).toBe(25);
+//   });
+// });
